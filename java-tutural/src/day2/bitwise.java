4321dc7ff0a633b5721d36a3a20b3c6e17230eb3
Nithin Jai <nithinjai80@gmail.com>
2026-07-10 11:58:31 +0530

    op

---
+++ java-tutural/src/day2/bitwise.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class bitwise {
+    public static  void main(String[] args){
+        int a=9;
+        int b=4;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(a<<b);
+        System.out.println(a>>3);
+    }
+}
