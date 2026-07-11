360e13a16c97d87a132fbc910efb1af9ca5dd8a3
Nithin Jai <nithinjai80@gmail.com>
2026-07-11 15:41:58 +0530

    java

---
+++ day2/bitwise.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class bitwise {
+    public static  void main(String[] args){
+        int a=9;
+        int b=4;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(a<<b);
+        System.out.println(a>>3);
+        System.out.println(5<<1);
+    }
+}
---
+++ day2/relationopration.java
@@ -0,0 +1,16 @@
+package day2;
+
+import java.util.*;
+public class relationopration {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
